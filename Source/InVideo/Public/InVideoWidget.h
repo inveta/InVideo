57c655e4430ae81e234fc3789909a073b1767470
g0415shenw <411320557@qq.com>
2023-06-11 01:03:18 +0800

    重大更新，实现视频打开和关闭全异步化，不会再阻塞蓝图线程

--- Source/InVideo/Public/InVideoWidget.h
+++ Source/InVideo/Public/InVideoWidget.h
@@ -16,39 +16,20 @@
 
 #include "InVideoWidget.generated.h"
 
+DECLARE_DYNAMIC_DELEGATE(FDelegatePlaySucceeded);
+DECLARE_DYNAMIC_DELEGATE(FDelegatePlayFailed);
+DECLARE_DYNAMIC_DELEGATE(FDelegateFirstFrame);
 
-/**
- * 
- */
-UCLASS()
-class INVIDEO_API UInVideoWidget : public UUserWidget ,public FRunnable
+
+class VideoPlay :public FRunnable
 {
-	GENERATED_BODY()
-	
 public:
-	DECLARE_DYNAMIC_DELEGATE(FDelegatePlaySucceeded);
-	DECLARE_DYNAMIC_DELEGATE(FDelegatePlayFailed);
-	DECLARE_DYNAMIC_DELEGATE(FDelegateFirstFrame);
-
-	UFUNCTION(BlueprintCallable, Category = "InVideo")
-    void StartPlay(const FString VideoURL, FDelegatePlayFailed Failed, FDelegateFirstFrame FirstFrame,
-		const bool RealMode = true,const int Fps = 25);
-
-	UFUNCTION(BlueprintCallable, Category = "InVideo")
+	void StartPlay(const FString VideoURL, FDelegatePlayFailed Failed, FDelegateFirstFrame FirstFrame,
+		const bool RealMode = true, const int Fps = 25, UInVideoWidget* widget=nullptr);
 	void StopPlay();
-
-	UPROPERTY(BlueprintReadOnly, Category = "InVideo")
-	UTexture2D* VideoTexture = nullptr;
-
-	UPROPERTY(BlueprintReadWrite, Meta = (BindWidget),Category = "InVideo")
-	UImage* ImageVideo;
 public:
-	virtual void NativeConstruct() override;
-	virtual void NativeDestruct() override;
-
-public:
-	bool Init() override; 
-	uint32 Run() override; 
+	bool Init() override;
+	uint32 Run() override;
 	void Stop() override;
 	void Exit() override;
 private:
@@ -56,6 +37,9 @@ private:
 	void UpdateTextureRegions(UTexture2D* Texture, int32 MipIndex, uint32 NumRegions, FUpdateTextureRegion2D* Regions, uint32 SrcPitch, uint32 SrcBpp, uint8* SrcData, bool bFreeData);
 	void NotifyFailed();
 	void NotifyFirstFrame();
+public:
+	UTexture2D* VideoTexture = nullptr;
+	UInVideoWidget* m_widget = nullptr;
 private:
 	FRunnableThread* m_Thread = nullptr;
 	TAtomic<bool> m_Stopping = false;
@@ -75,11 +59,39 @@ private:
 		cv::VideoCapture m_Stream;
 		cv::Mat m_Frame;
 	};
-	WrapOpenCv *m_WrapOpenCv = nullptr;
+	WrapOpenCv* m_WrapOpenCv = nullptr;
 
 	FVector2D m_VideoSize = FVector2D(0, 0);
 	FUpdateTextureRegion2D* m_VideoUpdateTextureRegion = nullptr;
 	FTexture2DResource* m_Texture2DResource = nullptr;
 	TArray64<FColor> Data;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class INVIDEO_API UInVideoWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+
+	UFUNCTION(BlueprintCallable, Category = "InVideo")
+    void StartPlay(const FString VideoURL, FDelegatePlayFailed Failed, FDelegateFirstFrame FirstFrame,
+		const bool RealMode = true,const int Fps = 25);
+
+	UFUNCTION(BlueprintCallable, Category = "InVideo")
+	void StopPlay();
+
+	UPROPERTY(BlueprintReadWrite, Meta = (BindWidget),Category = "InVideo")
+	UImage* ImageVideo;
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+private:
+	TUniquePtr<VideoPlay> m_VideoPlayPtr;
 };
 
